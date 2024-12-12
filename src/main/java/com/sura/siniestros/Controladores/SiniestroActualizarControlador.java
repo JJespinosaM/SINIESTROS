e112f46513395aa8679b241cd1b2e49095a8c8a7
Juan Espinosa <josemu008@gmail.com>
2024-12-12 10:44:49 -0500

    Prueba de rama 3

--- src/main/java/com/sura/siniestros/Controladores/SiniestroActualizarControlador.java
+++ src/main/java/com/sura/siniestros/Controladores/SiniestroActualizarControlador.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/siniestro")
+@RequestMapping("/api/actualizar/siniestro")
 public class SiniestroActualizarControlador {
 
     @Autowired
